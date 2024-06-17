cd2b08b15b862cc93878d890c5151095bf767e0b
naswa <endabelyuproject@gmail.com>
2024-06-17 08:50:33 +0700

    feat(home): add style font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./**/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
+      display: ['Oswald'],
+      body: ['"Open Sans"'],
+      roboto: ['"Roboto"', 'sans-serif'],
+      raleway: ['"Raleway"', 'sans-serif'],
+    },
   },
   plugins: [],
 };
